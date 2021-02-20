3bff503717943ab4bed17e043a245cb2da4611c0
Julian Hess <jhess@broadinstitute.org>
2021-02-19 23:27:09 -0500

    Initial commit of RBP integration

--- hapaseg/load.py
+++ hapaseg/load.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats as s
 
-from capy import txt
+from capy import txt, df
 from itertools import zip_longest
 
 _chrmap = dict(zip(["chr" + str(x) for x in list(range(1, 23)) + ["X", "Y"]], range(1, 25)))
@@ -10,13 +10,14 @@ _chrmap = dict(zip(["chr" + str(x) for x in list(range(1, 23)) + ["X", "Y"]], ra
 class HapasegReference:
     def __init__(self,
       phased_VCF = "test.vcf",
+      readbacked_phased_VCF = None,
       allele_counts = "3328.tumor.tsv",
       coverage = None,
       cytoband_file = "cytoBand.txt"
     ):
         #
         # load in VCF
-        self.allele_counts = self.load_VCF(phased_VCF, allele_counts)
+        self.allele_counts = self.load_VCF(phased_VCF, allele_counts, readbacked_phased_VCF)
 
         #
         # load in coverage; merge with allele counts
@@ -26,13 +27,24 @@ class HapasegReference:
         self.chromosome_intervals = self.parse_cytoband(cytoband_file)
 
     @staticmethod
-    def load_VCF(VCF, allele_counts):
+    def load_VCF(VCF, allele_counts, RBP_VCF = None):
         P = pd.read_csv(VCF, sep = "\t", comment = "#", names = ["chr", "pos", "x", "ref", "alt", "y", "z", "a", "b", "hap"], header = None)
         P = P.loc[:, ~P.columns.str.match('^.$')]
 
         P = txt.parsein(P, 'hap', r'(.)[/|](.)', ["allele_A", "allele_B"]).astype({"allele_A" : int, "allele_B" : int })
         P["phased"] = P["hap"].str[1] == "|"
 
+        # XXX: one day, we may support loading VCFs with misphasing probabilities
+        #      already given. but today is not that day. this is just a placeholder
+        #      for values that may be filled in from read-backed phasing.
+        P["misphase_prob"] = np.nan
+
+        #
+        # merge with read-backed phasing VCF, if given
+        if RBP_VCF is not None:
+            P = HapasegReference.load_RBP_VCF(RBP_VCF, P)
+
+        #
         # pull in altcounts
         C = pd.read_csv(allele_counts, sep = "\t")
         P = P.merge(C, how = "inner", left_on = ["chr", "pos"], right_on = ["CONTIG", "POSITION"]).drop(columns = ["CONTIG", "POSITION"])
@@ -58,6 +70,51 @@ class HapasegReference:
         P[["CI_lo_hap", "median_hap", "CI_hi_hap"]] = CI
 
         return P
+
+    @staticmethod
+    def load_RBP_VCF(RBP_VCF, P):
+        P_RBP = pd.read_csv(RBP_VCF, sep = "\t", comment = "#", names = ["chr", "pos", "x", "ref", "alt", "y", "z", "a", "b", "hap"], header = None)
+        P_RBP = P_RBP.loc[:, ~P_RBP.columns.str.match('^.$')]
+
+        P_RBP = txt.parsein(P_RBP, 'hap', r'(.)[/|](.)(?::(\d+))?', ["allele_A", "allele_B", "PS"]).astype({"allele_A" : int, "allele_B" : int })
+        P_RBP = P_RBP.astype({"allele_A" : pd.Int8Dtype(), "allele_B" : pd.Int8Dtype() })
+        P_RBP["phased"] = P_RBP["hap"].str[1] == "|"
+        P_RBP = P_RBP.loc[P_RBP["phased"]].astype({ "PS" : int }).astype({ "PS" : pd.Int32Dtype() })
+
+        # compare RBP phasing orientation to imputed orientation within each RBP phase sets
+        # set imputed orientation to RBP orientation with max Jaccard similarity
+        P_RBP = P_RBP.merge(P.loc[:, ["chr", "pos", "allele_A", "allele_B"]].astype({"allele_A" : pd.Int8Dtype(), "allele_B" : pd.Int8Dtype() }), left_on = ["chr", "pos"], right_on = ["chr", "pos"], suffixes = ("_RBP", "_IMP"), how = "left")
+        P_RBP["misphase_prob"] = np.nan
+
+        PS_idx = P_RBP.groupby("PS").indices
+
+        for rng in PS_idx.values():
+            PS = P_RBP.iloc[rng]
+            RBP_f = PS.loc[:, ["allele_A_RBP", "allele_B_RBP"]].values
+            RBP_r = PS.loc[:, ["allele_B_RBP", "allele_A_RBP"]].values
+            IMP = PS.loc[:, ["allele_A_IMP", "allele_B_IMP"]].values
+
+            # sometimes we can RBP phase alleles that we couldn't impute; in that case,
+            # the imputed values will be pd.na. ignore these in the Jaccard comparison
+            nidx = ~pd.isnull(IMP)
+
+            if (RBP_f[nidx] & IMP[nidx]).sum()/(RBP_f[nidx] | IMP[nidx]).sum() < \
+               (RBP_r[nidx] & IMP[nidx]).sum()/(RBP_r[nidx] | IMP[nidx]).sum():
+                P_RBP.loc[rng, ["allele_A_IMP", "allele_B_IMP"]] = RBP_r
+            else:
+                P_RBP.loc[rng, ["allele_A_IMP", "allele_B_IMP"]] = RBP_f
+
+            P_RBP.loc[rng, "misphase_prob"] = 0
+
+        P_RBP = P_RBP.rename(columns = { "allele_A_IMP" : "allele_A", "allele_B_IMP" : "allele_B" })
+
+        # update imputed phasing to reflect RBF
+        mm = df.multimap(P.loc[:, ["chr", "pos"]], P_RBP.loc[:, ["chr", "pos"]]).dropna()
+        P.loc[mm.index, ["allele_A", "allele_B", "misphase_prob"]] = P_RBP.loc[mm, ["allele_A", "allele_B", "misphase_prob"]].values
+
+        # add sites recovered by RBF
+        mm = df.multimap(P_RBP.loc[:, ["chr", "pos"]], P.loc[:, ["chr", "pos"]])
+        return pd.concat([P, P_RBP.loc[mm[mm.isna()].index, P.columns]], ignore_index = True).sort_values(["chr", "pos"])
 
     @staticmethod
     def parse_cytoband(cytoband):
