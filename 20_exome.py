db5935d81b381350940b5eadb9399b86b5ecbe45
Julian Hess <jhess@broadinstitute.org>
2021-03-12 11:56:20 -0500

    Notes on debugging phase correction

--- 20_exome.py
+++ 20_exome.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats as s
+import sortedcontainers as sc
 import subprocess
 import sys
 from wolf import fc
@@ -71,6 +72,7 @@ import hapaseg
 import dask.distributed as dd
 
 c = dd.Client(n_workers = 36)
+c = dd.Client()
 
 refs = hapaseg.load.HapasegReference(phased_VCF = "exome/6_C1D1_META.eagle.vcf", allele_counts = "exome/6_C1D1_META.tumor.tsv")
 
@@ -86,3 +88,58 @@ allelic_segs = runner.run_all()
 
 #allelic_segs.to_pickle("exome/6_C1D1_META.allelic_segs.pickle")
 allelic_segs = pd.read_pickle("exome/6_C1D1_META.allelic_segs.pickle")
+
+#
+# debug why reverting intervals in F won't restore us to the original state
+
+refs = hapaseg.load.HapasegReference(phased_VCF = "exome/6_C1D1_META.eagle.vcf", allele_counts = "exome/6_C1D1_META.tumor.tsv")
+
+runner = hapaseg.run_allelic_MCMC.AllelicMCMCRunner(
+  refs.allele_counts.loc[refs.allele_counts["chr"] == 1],
+  refs.chromosome_intervals,
+  c,
+  #phase_correct = False,
+  misphase_prior = 3e-3,
+  #_ref_bias = 0.936 # tmp: will be automatically inferred later
+)
+
+self = runner
+
+#
+# code excised from run_allelic_MCMC
+
+from hapaseg import A_MCMC
+
+chunks = [slice(*x) for x in self.chunks[["start", "end"]].values]
+
+futures = self.client.map(self._run_on_chunks, chunks, P = self.P_shared)
+self.chunks["results"] = self.client.gather(futures)
+
+#
+# concatenate burned in chunks for each arm
+H = [None]*len(self.chunks["arm"].unique())
+for i, (arm, A) in enumerate(self.chunks.groupby("arm")):
+    # concatenate allele count dataframes
+    H[i] = A_MCMC(
+      pd.concat([x.P for x in A["results"]], ignore_index = True),
+      n_iter = self.n_iter,
+      phase_correct = self.phase_correct,
+      misphase_prior = self.misphase_prior,
+      ref_bias = self._ref_bias # TODO: infer dynamically from burnin chunks
+    )
+
+    # replicate constructor steps to define initial breakpoint set and
+    # marginal likelihood dict
+    breakpoints = [None]*len(A)
+    H[i].seg_marg_liks = sc.SortedDict()
+    for j, (_, _, start, _, r) in enumerate(A.itertuples()):
+        start -= A["start"].iloc[0]
+        breakpoints[j] = np.array(r.breakpoints) + start
+        for k, v in r.seg_marg_liks.items():
+            H[i].seg_marg_liks[k + start] = v
+    H[i].breakpoints = sc.SortedSet(np.hstack(breakpoints))
+
+    H[i].marg_lik = np.full(H[i].n_iter, np.nan)
+    H[i].marg_lik[0] = np.array(H[i].seg_marg_liks.values()).sum()
+
+H[0].run()
