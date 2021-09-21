9bc2c40f1ed2df2414d132edda6ca7c872b1a4d2
Julian Hess <jhess@broadinstitute.org>
2021-09-21 10:59:19 -0400

    Update SNP list

--- wolf/workflow.py
+++ wolf/workflow.py
@@ -21,7 +21,7 @@ phasing = wolf.ImportTask(
 # get het site coverage/genotypes from callstats
 hp = het_pulldown.get_het_coverage_from_callstats(
   callstats_file = "gs://fc-secure-66f5eeb9-27c4-4e5c-b9d6-0519aca5889d/pair/05bd347a/05bd347a-3da7-4d1f-9bc6-4375226a0cb4_de3962db-0bd7-4126-85d9-da9ffe131088.MuTect1.call_stats.txt",
-  common_snp_list = "gs://getzlab-workflows-reference_files-oa/hg38/gnomad/gnomAD_MAF10_50pct_45prob_hg38_fixed.txt",
+  common_snp_list = "gs://getzlab-workflows-reference_files-oa/hg38/gnomad/gnomAD_MAF10_50pct_45prob_hg38_final.txt",
  ref_fasta = "gs://getzlab-workflows-reference_files-oa/hg38/gdc/GRCh38.d1.vd1.fa",
  ref_fasta_idx = "gs://getzlab-workflows-reference_files-oa/hg38/gdc/GRCh38.d1.vd1.fa.fai",
  ref_fasta_dict = "gs://getzlab-workflows-reference_files-oa/hg38/gdc/GRCh38.d1.vd1.dict",
