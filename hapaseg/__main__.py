307c26a4f93aea196a26a05e4535ed98b64c441c
Oliver Priebe <opriebe@broadinstitute.org>
2021-11-18 14:48:13 -0500

    added coverage parsers and calls
    for now the inputs and output processing is all handled within the respective
    modules. may want to later move these to the main method and also allow for
    object passing within the standalone run type

--- hapaseg/__main__.py
+++ hapaseg/__main__.py
@@ -14,101 +14,125 @@ from capy import mut
 from .load import HapasegReference
 from .run_allelic_MCMC import AllelicMCMCRunner
 from .allelic_MCMC import A_MCMC
+from .run_coverage_MCMC import CoverageMCMCRunner
+from .coverage_DP import Coverage_DP
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = "Call somatic copynumber alterations taking advantage of SNP phasing")
+    parser = argparse.ArgumentParser(description="Call somatic copynumber alterations taking advantage of SNP phasing")
 
-    parser.add_argument("--output_dir", default = ".")
+    parser.add_argument("--output_dir", default=".")
 
-    subparsers = parser.add_subparsers(dest = "command")
+    subparsers = parser.add_subparsers(dest="command")
 
     ## run
-    standalone = subparsers.add_parser("run", help = "Run HapASeg as a standalone module")
-    standalone.add_argument("--n_workers", default = multiprocessing.cpu_count() - 1)
-    standalone.add_argument("--n_iter", default = 20000)
+    standalone = subparsers.add_parser("run", help="Run HapASeg as a standalone module")
+    standalone.add_argument("--n_workers", default=multiprocessing.cpu_count() - 1)
+    standalone.add_argument("--n_iter", default=20000)
 
     input_group = standalone.add_mutually_exclusive_group(
-      required = True
-      #"Mutually exclusive inputs",
-      #"Hapaseg can either take an already phased VCF annotated with alt/refcounts, or a MuTect callstats file. In the case of the latter, Hapaseg will perform phasing.",
+        required=True
+        # "Mutually exclusive inputs",
+        # "Hapaseg can either take an already phased VCF annotated with alt/refcounts, or a MuTect callstats file. In the case of the latter, Hapaseg will perform phasing.",
     )
     input_group.add_argument("--phased_VCF")
     input_group.add_argument("--callstats_file")
 
     # if we are performing phasing ourselves, we need these reference files
     phasing_ref_group = standalone.add_argument_group(
-      "Phasing ref. files",
-      "Required files for Hapaseg to pull down het sites and impute phasing.",
+        "Phasing ref. files",
+        "Required files for Hapaseg to pull down het sites and impute phasing.",
     )
     phasing_ref_group.add_argument("--phasing_ref_panel_dir")
     phasing_ref_group.add_argument("--phasing_genetic_map_file")
     phasing_ref_group.add_argument("--SNP_list")
     phasing_ref_group.add_argument("--ref_fasta")
-    phasing_ref_group.add_argument("--bam", help = "BAM to use for read-backed phasing")
-    phasing_ref_group.add_argument("--bai", help = "BAI to use for read-backed phasing")
+    phasing_ref_group.add_argument("--bam", help="BAM to use for read-backed phasing")
+    phasing_ref_group.add_argument("--bai", help="BAI to use for read-backed phasing")
 
     # if we are taking a pre-phased VCF, we also may want a read-backed phased VCF
-    prephased_group = standalone.add_argument_group( 
-      "Inputs if phasing has already been imputed",
+    prephased_group = standalone.add_argument_group(
+        "Inputs if phasing has already been imputed",
     )
-    prephased_group.add_argument("--read_backed_phased_VCF", help = "Optional.")
-    prephased_group.add_argument("--allele_counts_T", help = "Required.")
-    prephased_group.add_argument("--allele_counts_N", help = "Required.")
+    prephased_group.add_argument("--read_backed_phased_VCF", help="Optional.")
+    prephased_group.add_argument("--allele_counts_T", help="Required.")
+    prephased_group.add_argument("--allele_counts_N", help="Required.")
 
     ai_seg_params = standalone.add_argument_group(
-      "Parameters for allelic imbalance segmentation",
+        "Parameters for allelic imbalance segmentation",
     )
-    ai_seg_params.add_argument("--phase_correct", action = "store_true")
-    ai_seg_params.add_argument("--misphase_prior", default = "0.001")
+    ai_seg_params.add_argument("--phase_correct", action="store_true")
+    ai_seg_params.add_argument("--misphase_prior", default="0.001")
 
     ref_group = standalone.add_argument_group(
-      "Required reference files",
+        "Required reference files",
     )
-    ref_group.add_argument("--cytoband_file", required = True)
+    ref_group.add_argument("--cytoband_file", required=True)
 
     ## load
-    scatter = subparsers.add_parser("load", help = "Load in phased VCF")
-    scatter.add_argument("--chunk_size", default = 5000) 
-    scatter.add_argument("--phased_VCF", required = True)
+    scatter = subparsers.add_parser("load", help="Load in phased VCF")
+    scatter.add_argument("--chunk_size", default=5000)
+    scatter.add_argument("--phased_VCF", required=True)
     scatter.add_argument("--read_backed_phased_VCF")
-    scatter.add_argument("--allele_counts_T", required = True)
-    scatter.add_argument("--allele_counts_N", required = True)
-    scatter.add_argument("--cytoband_file", required = True)
+    scatter.add_argument("--allele_counts_T", required=True)
+    scatter.add_argument("--allele_counts_N", required=True)
+    scatter.add_argument("--cytoband_file", required=True)
 
     ## amcmc
-    amcmc = subparsers.add_parser("amcmc", help = "Run allelic MCMC on a range of SNPs")
+    amcmc = subparsers.add_parser("amcmc", help="Run allelic MCMC on a range of SNPs")
 
     input_group = amcmc.add_mutually_exclusive_group(
-      required = True
+        required=True
     )
     input_group.add_argument("--snp_dataframe")
     input_group.add_argument("--amcmc_object")
 
-    amcmc.add_argument("--start", default = 0)
-    amcmc.add_argument("--end", default = -1)
-    amcmc.add_argument("--stop_after_burnin", action = "store_true")
-    amcmc.add_argument("--ref_bias", default = 1.0)
-    amcmc.add_argument("--n_iter", default = 20000)
+    amcmc.add_argument("--start", default=0)
+    amcmc.add_argument("--end", default=-1)
+    amcmc.add_argument("--stop_after_burnin", action="store_true")
+    amcmc.add_argument("--ref_bias", default=1.0)
+    amcmc.add_argument("--n_iter", default=20000)
 
     ## concat
-    concat = subparsers.add_parser("concat", help = "Concatenate burned-in chunks")
-    concat.add_argument("--chunks", required = True, nargs = "+")
-    concat.add_argument("--scatter_intervals", required = True)
+    concat = subparsers.add_parser("concat", help="Concatenate burned-in chunks")
+    concat.add_argument("--chunks", required=True, nargs="+")
+    concat.add_argument("--scatter_intervals", required=True)
 
     ## DP (TODO: will include gather step)
-    dp = subparsers.add_parser("dp", help = "Run DP clustering on allelic imbalance segments")
+    dp = subparsers.add_parser("dp", help="Run DP clustering on allelic imbalance segments")
+
+    coverage_mcmc = subparsers.add_parser("coverage_mcmc", help="Run TCR segmentation on allelic imbalance clusters")
+    coverage_mcmc.add_argument("--coverage_csv",
+                               help="csv file containing '['chr', 'start', 'end', 'covcorr', 'covraw'] data")
+    coverage_mcmc.add_argument("--allelic_clusters_object",
+                               help="npy file containing allelic dp segs-to-clusters results")
+    coverage_mcmc.add_argument("--SNPs_pickle", help="pickled dataframe containing SNPs")
+    coverage_mcmc.add_argument("--covariate_dir",
+                               help="path to covariate directory with covariates all in pickled files")
+    coverage_mcmc.add_argument("--num_draws",
+                               help="number of draws to take from coverage segmentation MCMC", default=50)
+    coverage_mcmc.add_argument("--allelic_sample",
+                               help="index of sample clustering from allelic DP to use as seed for segmentation",
+                               default=None)
+
+    coverage_dp = subparsers.add_parser("coverage_dp", help="Run DP clustering on coverage segmentations")
+    coverage_dp.add_argument("--num_segmentation_samples", help="number of segmentation samples to use")
+    coverage_dp.add_argument("--num_draws",
+                             help="number of thinned draws from the coverage dp to take after burn in")
+    coverage_dp.add_argument("segmentation_h5_path", help="segmentation mcmc output file path")
 
     args = parser.parse_args()
 
     # validate arguments
-#    if args.phased_VCF is not None:
-#        if args.allele_counts_T is None or args.allele_counts_N is None:
-#            raise ValueError("Must supply allele counts from HetPulldown if not imputing phasing")
-#    # TODO: if callstats_file is given, then all phasing_ref_group args must be present
+    #    if args.phased_VCF is not None:
+    #        if args.allele_counts_T is None or args.allele_counts_N is None:
+    #            raise ValueError("Must supply allele counts from HetPulldown if not imputing phasing")
+    #    # TODO: if callstats_file is given, then all phasing_ref_group args must be present
 
     return args
 
-def main(): 
+
+def main():
     args = parse_args()
 
     if not os.path.isdir(args.output_dir):
@@ -116,21 +140,21 @@ def main():
     output_dir = os.path.realpath(args.output_dir)
 
     if args.command == "run":
-        dask_client = dd.Client(n_workers = int(args.n_workers))
+        dask_client = dd.Client(n_workers=int(args.n_workers))
 
         refs = HapasegReference(
-          phased_VCF = args.phased_VCF,
-          readbacked_phased_VCF = args.read_backed_phased_VCF,
-          allele_counts = args.allele_counts_T,
-          allele_counts_N = args.allele_counts_N,
-          cytoband_file = args.cytoband_file
+            phased_VCF=args.phased_VCF,
+            readbacked_phased_VCF=args.read_backed_phased_VCF,
+            allele_counts=args.allele_counts_T,
+            allele_counts_N=args.allele_counts_N,
+            cytoband_file=args.cytoband_file
         )
 
         runner = AllelicMCMCRunner(
-          refs.allele_counts,
-          refs.chromosome_intervals,
-          dask_client,
-          phase_correct = args.phase_correct
+            refs.allele_counts,
+            refs.chromosome_intervals,
+            dask_client,
+            phase_correct=args.phase_correct
         )
 
         allelic_segs = runner.run_all()
@@ -143,39 +167,39 @@ def main():
     elif args.command == "load":
         # load from VCF
         refs = HapasegReference(
-          phased_VCF = args.phased_VCF,
-          readbacked_phased_VCF = args.read_backed_phased_VCF,
-          allele_counts = args.allele_counts_T,
-          allele_counts_N = args.allele_counts_N,
-          cytoband_file = args.cytoband_file
+            phased_VCF=args.phased_VCF,
+            readbacked_phased_VCF=args.read_backed_phased_VCF,
+            allele_counts=args.allele_counts_T,
+            allele_counts_N=args.allele_counts_N,
+            cytoband_file=args.cytoband_file
         )
 
         # create chunks
-        t = mut.map_mutations_to_targets(refs.allele_counts, refs.chromosome_intervals, inplace = False)
-        groups = t.groupby(t).apply(lambda x : [x.index.min(), x.index.max()]).to_frame(name = "bdy")
-        groups["ranges"] = groups["bdy"].apply(lambda x : np.r_[x[0]:x[1]:args.chunk_size, x[1]])
+        t = mut.map_mutations_to_targets(refs.allele_counts, refs.chromosome_intervals, inplace=False)
+        groups = t.groupby(t).apply(lambda x: [x.index.min(), x.index.max()]).to_frame(name="bdy")
+        groups["ranges"] = groups["bdy"].apply(lambda x: np.r_[x[0]:x[1]:args.chunk_size, x[1]])
         chunks = pd.DataFrame(
-          np.vstack([
-            np.hstack(np.broadcast_arrays(k, np.c_[y[0:-1], y[1:]]))
-            for k, y in groups["ranges"].iteritems()
-          ])[:, 1:],
-          columns = ["arm", "start", "end"]
+            np.vstack([
+                np.hstack(np.broadcast_arrays(k, np.c_[y[0:-1], y[1:]]))
+                for k, y in groups["ranges"].iteritems()
+            ])[:, 1:],
+            columns=["arm", "start", "end"]
         )
 
         # save to disk
         refs.allele_counts.to_pickle(output_dir + "/allele_counts.pickle")
         refs.chromosome_intervals.to_pickle(output_dir + "/chrom_int.pickle")
-        chunks.to_csv(output_dir + "/scatter_chunks.tsv", sep = "\t", index = False)
+        chunks.to_csv(output_dir + "/scatter_chunks.tsv", sep="\t", index=False)
 
     elif args.command == "amcmc":
         # loading from SNP dataframe produced by `hapaseg load`
         if args.snp_dataframe is not None:
             P = pd.read_pickle(args.snp_dataframe)
             H = A_MCMC(
-              P.iloc[int(args.start):int(args.end)],
-              quit_after_burnin = args.stop_after_burnin,
-              ref_bias = float(args.ref_bias),
-              n_iter = int(args.n_iter)
+                P.iloc[int(args.start):int(args.end)],
+                quit_after_burnin=args.stop_after_burnin,
+                ref_bias=float(args.ref_bias),
+                n_iter=int(args.n_iter)
             )
 
         # loading from allelic MCMC results object produced by `hapaseg amcmc`
@@ -197,7 +221,7 @@ def main():
     elif args.command == "concat":
         #
         # load scatter intervals
-        intervals = pd.read_csv(args.scatter_intervals, sep = "\t")
+        intervals = pd.read_csv(args.scatter_intervals, sep="\t")
 
         if len(intervals) != len(args.chunks):
             raise ValueError("Length mismatch in supplied chunks and interval file!")
@@ -208,21 +232,22 @@ def main():
             with open(chunk_path, "rb") as f:
                 chunk = pickle.load(f)
             R.append(chunk)
-        R = pd.DataFrame({ "results" : R })
+        R = pd.DataFrame({"results": R})
 
         # ensure results are in the correct order
-        R["first"] = R["results"].apply(lambda x : x.P.loc[0, "index"])
-        R = R.sort_values("first", ignore_index = True)
+        R["first"] = R["results"].apply(lambda x: x.P.loc[0, "index"])
+        R = R.sort_values("first", ignore_index=True)
 
         # concat with intervals
-        R = pd.concat([R, intervals], axis = 1).drop(columns = ["first"])
+        R = pd.concat([R, intervals], axis=1).drop(columns=["first"])
 
         #
         # compute reference bias
         X = []
         j = 0
         for chunk in R["results"]:
-            bpl = np.array(chunk.breakpoints); bpl = np.c_[bpl[0:-1], bpl[1:]]
+            bpl = np.array(chunk.breakpoints);
+            bpl = np.c_[bpl[0:-1], bpl[1:]]
 
             for i, (st, en) in enumerate(bpl):
                 x = chunk.P.iloc[st:en].groupby("allele_A")[["MIN_COUNT", "MAJ_COUNT"]].sum()
@@ -235,20 +260,21 @@ def main():
         g = X.groupby("idx").size() == 2
         Y = X.loc[X["idx"].isin(g[g].index)]
 
-        f = np.zeros([len(Y)//2, 100])
-        l = np.zeros([len(Y)//2, 2])
+        f = np.zeros([len(Y) // 2, 100])
+        l = np.zeros([len(Y) // 2, 2])
         for i, (_, g) in enumerate(Y.groupby("idx")):
-            f[i, :] = s.beta.rvs(g.loc[0, "MIN_COUNT"] + 1, g.loc[0, "MAJ_COUNT"] + 1, size = 100)/s.beta.rvs(g.loc[1, "MIN_COUNT"] + 1, g.loc[1, "MAJ_COUNT"] + 1, size = 100)
+            f[i, :] = s.beta.rvs(g.loc[0, "MIN_COUNT"] + 1, g.loc[0, "MAJ_COUNT"] + 1, size=100) / s.beta.rvs(
+                g.loc[1, "MIN_COUNT"] + 1, g.loc[1, "MAJ_COUNT"] + 1, size=100)
             l[i, :] = np.r_[
-              ss.betaln(g.loc[0, "MIN_COUNT"] + 1, g.loc[0, "MAJ_COUNT"] + 1),
-              ss.betaln(g.loc[1, "MIN_COUNT"] + 1, g.loc[1, "MAJ_COUNT"] + 1),
+                ss.betaln(g.loc[0, "MIN_COUNT"] + 1, g.loc[0, "MAJ_COUNT"] + 1),
+                ss.betaln(g.loc[1, "MIN_COUNT"] + 1, g.loc[1, "MAJ_COUNT"] + 1),
             ]
 
         # weight mean by negative log marginal likelihoods
         # take smaller of the two likelihoods to account for power imbalance
-        w = np.min(-l, 1, keepdims = True)
+        w = np.min(-l, 1, keepdims=True)
 
-        ref_bias = (f*w).sum()/(100*w.sum())
+        ref_bias = (f * w).sum() / (100 * w.sum())
 
         with open(output_dir + "/ref_bias.txt", "w") as f:
             f.write(str(ref_bias))
@@ -257,13 +283,13 @@ def main():
         # concat burned in chunks for each arm
         for arm, Ra in R.groupby("arm"):
             A = A_MCMC(
-              pd.concat([x.P for x in Ra["results"]], ignore_index = True),
-              # other class properties will be filled in with their correct values later
+                pd.concat([x.P for x in Ra["results"]], ignore_index=True),
+                # other class properties will be filled in with their correct values later
             )
 
             # replicate constructor steps to define initial breakpoint set and
             # marginal likelihood dict
-            breakpoints = [None]*len(Ra)
+            breakpoints = [None] * len(Ra)
             A.seg_marg_liks = sc.SortedDict()
             for j, Ras in enumerate(Ra.itertuples()):
                 start = Ras.start - Ra["start"].iloc[0]
@@ -272,11 +298,26 @@ def main():
                     A.seg_marg_liks[k + start] = v
             A.breakpoints = sc.SortedSet(np.hstack(breakpoints))
 
-            A.marg_lik = np.full(A.n_iter, np.nan) # n_iter and size of this array will be reset later
+            A.marg_lik = np.full(A.n_iter, np.nan)  # n_iter and size of this array will be reset later
             A.marg_lik[0] = np.array(A.seg_marg_liks.values()).sum()
 
             with open(output_dir + f"/AMCMC-arm{arm}.pickle", "wb") as f:
                 pickle.dump(A, f)
+
+    elif args.command == "coverage_mcmc":
+        cov_mcmc_runner = CoverageMCMCRunner(args.coverage_csv,
+                                             args.allelic_clusters_object,
+                                             args.SNPs_pickle,
+                                             args.covariate_dir,
+                                             args.num_draws,
+                                             args.allelic_sample)
+        cov_mcmc_runner.run()
+
+    elif args.command == "coverage_dp":
+        cov_dp_runner = Coverage_DP(args.segmentation_h5_path)
+        cov_dp_runner.run_dp(args.num_segmentation_samples, args.num_draws)
+        with open(output_dir + f"/Cov_DP_object.pickle", "wb") as f:
+            pickle.dump(cov_dp_runner, f)
 
 
 if __name__ == "__main__":
