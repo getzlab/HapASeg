5fcee32136c6cdf737450512c02480745b00a673
Julian Hess <jhess@broadinstitute.org>
2022-04-18 23:52:13 -0400

    Bump docker

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -105,5 +105,5 @@ class Hapaseg_allelic_DP(wolf.Task):
       "SNP_plot" : "figures/SNPs.png",
       "seg_plot" : "figures/segs_only.png",
     }
-    docker = "gcr.io/broad-getzlab-workflows/hapaseg:all_SNPs_v608"
+    docker = "gcr.io/broad-getzlab-workflows/hapaseg:all_SNPs_v611"
     resources = { "mem" : "5G" }
