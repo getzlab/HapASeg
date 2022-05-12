ee1f6b56b58c1f42c68372370a152254fdbe76eb
Julian Hess <jhess@broadinstitute.org>
2022-05-12 16:01:44 -0400

    Add log exposure to Poisson regression

--- hapaseg/model_optimizers.py
+++ hapaseg/model_optimizers.py
@@ -2,14 +2,15 @@ import numpy as np
 
 
 class PoissonRegression:
-    def __init__(self, r, C, Pi):
+    def __init__(self, r, C, Pi, log_exposure = 0):
         self.r = r
         self.C = C
         self.Pi = Pi
+        self.log_exposure = log_exposure
 
         self.mu = np.log(r.mean() * np.ones([Pi.shape[1], 1]))
         self.beta = np.ones([C.shape[1], 1])
-        self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu)
+        self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu + self.log_exposure)
 
     # mu gradient
     def gradmu(self):
@@ -33,7 +34,7 @@ class PoissonRegression:
 
     def NR_poisson(self):
         for i in range(100):
-            self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu)
+            self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu + self.log_exposure)
             gmu = self.gradmu()
             gbeta = self.gradbeta()
             grad = np.r_[gmu, gbeta]
