5718c707f232112b708b21f565f16f4ee4d69954
Julian Hess <jhess@broadinstitute.org>
2022-06-14 12:58:21 -0400

    Allow PoisRegr priors to be specified; allow running without intercept

--- hapaseg/model_optimizers.py
+++ hapaseg/model_optimizers.py
@@ -2,22 +2,25 @@ import numpy as np
 
 
 class PoissonRegression:
-    def __init__(self, r, C, Pi, log_exposure = 0, log_offset = 0):
+    def __init__(self, r, C, Pi,
+      log_exposure = 0, log_offset = 0, intercept = True,
+      mumu = 0, musig2 = 10, betamu = None, betasiginv = None):
         self.r = r
         self.C = C
         self.Pi = Pi
         self.log_exposure = log_exposure
         self.log_offset = log_offset
+        self.intercept = intercept
 
         self.mu = np.log(r.mean() * np.ones([Pi.shape[1], 1])) - self.log_exposure
         self.beta = np.zeros([C.shape[1], 1])
         self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu + self.log_exposure + self.log_offset)
 
         # prior parameters
-        self.mumu = 0
-        self.musig2 = 1
-        self.betamu = np.zeros_like(self.beta)
-        self.betasiginv = np.eye(len(self.beta))
+        self.mumu = mumu
+        self.musig2 = musig2
+        self.betamu = np.zeros_like(self.beta) if betamu is None else betamu
+        self.betasiginv = 1/np.sqrt(10)*np.eye(len(self.beta)) if betasiginv is None else betasiginv
 
     # mu gradient
     def gradmu(self):
@@ -42,28 +45,43 @@ class PoissonRegression:
     def NR_poisson(self):
         for i in range(100):
             self.e_s = np.exp(self.C @ self.beta + self.Pi @ self.mu + self.log_exposure + self.log_offset)
-            gmu = self.gradmu()
             gbeta = self.gradbeta()
-            grad = np.r_[gmu, gbeta]
+            if self.intercept:
+                gmu = self.gradmu()
+                grad = np.r_[gmu, gbeta]
+            else:
+                grad = gbeta
 
-            hmu = self.hessmu()
             hbeta = self.hessbeta()
-            hmubeta = self.hessmubeta()
-            H = np.r_[np.c_[hmu, hmubeta.T], np.c_[hmubeta, hbeta]]
+            if self.intercept:
+                hmubeta = self.hessmubeta()
+                hmu = self.hessmu()
+                H = np.r_[np.c_[hmu, hmubeta.T], np.c_[hmubeta, hbeta]]
+            else:
+                H = hbeta
 
             delta = np.linalg.inv(H) @ grad
-            self.mu -= delta[0:len(self.mu)]
-            self.beta -= delta[len(self.mu):]
+            if self.intercept:
+                self.mu -= delta[0:len(self.mu)]
+                self.beta -= delta[len(self.mu):]
+            else:
+                self.beta -= delta
 
             if np.linalg.norm(grad) < 1e-5:
                 break
 
     def fit(self):
         self.NR_poisson()
-        return self.mu, self.beta
+        if self.intercept:
+            return self.mu, self.beta
+        else:
+            return self.beta
 
     def hess(self):
-        hmu = self.hessmu()
         hbeta = self.hessbeta()
-        hmubeta = self.hessmubeta()
-        return np.r_[np.c_[hmu, hmubeta.T], np.c_[hmubeta, hbeta]]
+        if self.intercept:
+            hmu = self.hessmu()
+            hmubeta = self.hessmubeta()
+            return np.r_[np.c_[hmu, hmubeta.T], np.c_[hmubeta, hbeta]]
+        else:
+            return hbeta
