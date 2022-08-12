ce800eed762b8dd748a5c7c7c938dfeed188fcfd
Julian Hess <jhess@broadinstitute.org>
2022-08-12 12:55:47 -0400

    Disable init_prior in CMCMC

--- hapaseg/NB_coverage_MCMC.py
+++ hapaseg/NB_coverage_MCMC.py
@@ -161,18 +161,8 @@ class AllelicCluster:
 		return res.params[0], -np.log(res.params[1])
 
 	def lnp_init(self):
-		# run plain Poisson regression without fitting beta to get mu prior
-		prior = PoissonRegression(
-		  self.r,
-		  np.zeros([len(self.r), 0]), # don't fit beta
-		  np.ones_like(self.r),
-		  log_exposure = np.log(self.bin_exposure),
-		  log_offset = self.C@self.beta # beta included as offset
-		)
-		prior.fit()
-
 		# now fit LNP
-		lnp = CovLNP_NR_prior(self.r, self.beta, self.C, exposure = np.log(self.bin_exposure), init_prior=True, lamda = self.lamda, mu_prior = prior.mu[0, 0], alpha_prior = 1, beta_prior = 5e-2)
+		lnp = CovLNP_NR_prior(self.r, self.beta, self.C, exposure = np.log(self.bin_exposure), init_prior=False, lamda = self.lamda, mu_prior = self.mu, alpha_prior = 1, beta_prior = 5e-2)
 		return lnp.fit()
 
 	# statsmodels NB BFGS optimizer is more stable than NR so we will use it until migration to LNP
@@ -199,14 +189,14 @@ class AllelicCluster:
 
 		# cache miss; compute values
 		#lnp = CovLNP_NR(self.r[ind[0]:ind[1]], self.beta, self.C[ind[0]:ind[1]], exposure = np.log(self.bin_exposure) + self.mu)
-		lnp = CovLNP_NR_prior(self.r[ind[0]:ind[1]], self.beta, self.C[ind[0]:ind[1]], exposure = np.log(self.bin_exposure), mu_prior = 0, lamda = self.lamda, alpha_prior = self.alpha_prior, beta_prior = self.beta_prior, init_prior = True)
+		lnp = CovLNP_NR_prior(self.r[ind[0]:ind[1]], self.beta, self.C[ind[0]:ind[1]], exposure = np.log(self.bin_exposure), mu_prior = self.mu, lamda = self.lamda, alpha_prior = self.alpha_prior, beta_prior = self.beta_prior, init_prior = False)
 		
 		try:	
 			res =  lnp.fit(ret_hess=ret_hess)
 		except:
 			# try adding some jitter
 			try:
-				lnp = CovLNP_NR_prior(self.r[ind[0]:ind[1]], self.beta, self.C[ind[0]:ind[1]], exposure = np.log(self.bin_exposure) + self.mu, mu_prior = 0, lamda = self.lamda, alpha_prior = self.alpha_prior, beta_prior = self.beta_prior, extra_roots=True)
+				lnp = CovLNP_NR_prior(self.r[ind[0]:ind[1]], self.beta, self.C[ind[0]:ind[1]], exposure = np.log(self.bin_exposure), mu_prior = self.mu, lamda = self.lamda, alpha_prior = self.alpha_prior, beta_prior = self.beta_prior, extra_roots=True, init_prior = False)
 				res =  lnp.fit(ret_hess=ret_hess)
 			except:
 				res = (np.nan, np.nan, np.full((2,2), np.nan))
@@ -241,7 +231,7 @@ class AllelicCluster:
 	def ll_segment(self, ind, mu_i, lgsigma):
 		exposure = np.log(self.bin_exposure)
 		mu_tot = mu_i
-		ll = covLNP_ll_prior(self.r[ind[0]:ind[1]], mu_tot, lgsigma, self.C[ind[0]:ind[1]], self.beta, exposure = exposure, mu_prior = mu_tot, lamda = self.lamda, alpha_prior=self.alpha_prior, beta_prior = self.beta_prior)
+		ll = covLNP_ll_prior(self.r[ind[0]:ind[1]], mu_tot, lgsigma, self.C[ind[0]:ind[1]], self.beta, exposure = exposure, mu_prior = self.mu, lamda = self.lamda, alpha_prior=self.alpha_prior, beta_prior = self.beta_prior)
 		#ll = covLNP_ll(self.r[ind[0]:ind[1]], mu_tot, lgsigma, self.C[ind[0]:ind[1]], self.beta, exposure = exposure)
 		return ll
 
