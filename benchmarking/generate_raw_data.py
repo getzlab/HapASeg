f02f0b89acb89da1f831988e83351d85abc52956
Oliver Priebe <opriebe@broadinstitute.org>
2022-12-01 15:51:35 +0000

    updating hapaseg raw data generation for normals
    normal preprocessing skips phasing and method specific preprocessing tasks

--- benchmarking/generate_raw_data.py
+++ benchmarking/generate_raw_data.py
@@ -98,8 +98,6 @@ class ASCAT_Preprocess_Callstats(wolf.Task):
 #
 ###########
 
-# Covcollect
-
 def Generate_Hapaseg_Raw_Data_Workflow(
              sample_name = None,
              bam = None,
@@ -108,6 +106,7 @@ def Generate_Hapaseg_Raw_Data_Workflow(
              vcf = None,
              target_list = None,
              wgs=True,
+             preprocess=True, # whether or not to do phasing and preprocessing tasks
              db_snp_vcf=None, # for facets variant filtering
              ascat_loci_list=None, # for ascat variant filtering
              upload_bucket_gs_path=None,
@@ -238,152 +237,144 @@ def Generate_Hapaseg_Raw_Data_Workflow(
       """,
       outputs = { "cs_gather" : "*mutect_callstats.tsv" }
     )
+    
+    if preprocess:
 
-    # use hapaseg callstats filtering to grab the genotypes
-    hapaseg_cs_task = HapASeg_Preprocess_Callstats(inputs = {
-                                        "callstats": m1_gather["cs_gather"],
-                                        "sample_name": sample_name
-                                        }
-                                    )
+        # use hapaseg callstats filtering to grab the genotypes
+        hapaseg_cs_task = HapASeg_Preprocess_Callstats(inputs = {
+                                            "callstats": m1_gather["cs_gather"],
+                                            "sample_name": sample_name
+                                            }
+                                        )
 
-    # phasing will be the same for all benchmarking runs so we will also cache this result
+        # phasing will be the same for all benchmarking runs so we will also cache this result
 
-    convert_task = wolf.Task(
-      name = "convert_het_pulldown",
-      inputs = {
-        "genotype_file" : hapaseg_cs_task["hapaseg_genotype"],
-        "sample_name" : "test", # TODO: allow to be specified
-        "ref_fasta" : localization_task["ref_fasta"],
-        "ref_fasta_idx" : localization_task["ref_fasta_idx"],
-        "ref_fasta_dict" : localization_task["ref_fasta_dict"],
-      }, 
-      script = r"""
-    set -x
-    bcftools convert --tsv2vcf ${genotype_file} -c CHROM,POS,AA -s ${sample_name} \
-      -f ${ref_fasta} -Ou -o all_chrs.bcf && bcftools index all_chrs.bcf
-    for chr in $(bcftools view -h all_chrs.bcf | ssed -nR '/^##contig/s/.*ID=(.*),.*/\1/p' | head -n24); do
-      bcftools view -Ou -r ${chr} -o ${chr}.chrsplit.bcf all_chrs.bcf && bcftools index ${chr}.chrsplit.bcf
-    done
-    """,
-      outputs = {
-        "bcf" : "*.chrsplit.bcf",
-        "bcf_idx" : "*.chrsplit.bcf.csi"
-      },
-      docker = "gcr.io/broad-getzlab-workflows/base_image:v0.0.5"
-    )
+        convert_task = wolf.Task(
+          name = "convert_het_pulldown",
+          inputs = {
+            "genotype_file" : hapaseg_cs_task["hapaseg_genotype"],
+            "sample_name" : "test", # TODO: allow to be specified
+            "ref_fasta" : localization_task["ref_fasta"],
+            "ref_fasta_idx" : localization_task["ref_fasta_idx"],
+            "ref_fasta_dict" : localization_task["ref_fasta_dict"],
+          }, 
+          script = r"""
+        set -x
+        bcftools convert --tsv2vcf ${genotype_file} -c CHROM,POS,AA -s ${sample_name} \
+          -f ${ref_fasta} -Ou -o all_chrs.bcf && bcftools index all_chrs.bcf
+        for chr in $(bcftools view -h all_chrs.bcf | ssed -nR '/^##contig/s/.*ID=(.*),.*/\1/p' | head -n24); do
+          bcftools view -Ou -r ${chr} -o ${chr}.chrsplit.bcf all_chrs.bcf && bcftools index ${chr}.chrsplit.bcf
+        done
+        """,
+          outputs = {
+            "bcf" : "*.chrsplit.bcf",
+            "bcf_idx" : "*.chrsplit.bcf.csi"
+          },
+          docker = "gcr.io/broad-getzlab-workflows/base_image:v0.0.5"
+        )
 
-    #
-    # ensure that BCFs/indices/reference BCFs are in the same order
-    @prefect.task
-    def order_indices(bcf_path, bcf_idx_path, localization_task):
-        # BCFs
-        F = pd.DataFrame(dict(bcf_path = bcf_path))
-        F = F.set_index(F["bcf_path"].apply(os.path.basename).str.replace(r"^((?:chr)?(?:[^.]+)).*", r"\1"))
+        #
+        # ensure that BCFs/indices/reference BCFs are in the same order
+        @prefect.task
+        def order_indices(bcf_path, bcf_idx_path, localization_task):
+            # BCFs
+            F = pd.DataFrame(dict(bcf_path = bcf_path))
+            F = F.set_index(F["bcf_path"].apply(os.path.basename).str.replace(r"^((?:chr)?(?:[^.]+)).*", r"\1"))
 
-        # indices
-        F2 = pd.DataFrame(dict(bcf_idx_path = bcf_idx_path))
-        F2 = F2.set_index(F2["bcf_idx_path"].apply(os.path.basename).str.replace(r"^((?:chr)?(?:[^.]+)).*", r"\1"))
+            # indices
+            F2 = pd.DataFrame(dict(bcf_idx_path = bcf_idx_path))
+            F2 = F2.set_index(F2["bcf_idx_path"].apply(os.path.basename).str.replace(r"^((?:chr)?(?:[^.]+)).*", r"\1"))
 
-        F = F.join(F2)
+            F = F.join(F2)
 
-        # prepend "chr" to F's index if it's missing
-        idx = ~F.index.str.contains("^chr")
-        if idx.any():
-            new_index = F.index.values
-            new_index[idx] = "chr" + F.index[idx]
-            F = F.set_index(new_index)
+            # prepend "chr" to F's index if it's missing
+            idx = ~F.index.str.contains("^chr")
+            if idx.any():
+                new_index = F.index.values
+                new_index[idx] = "chr" + F.index[idx]
+                F = F.set_index(new_index)
 
-        # reference panel BCFs
-        R = pd.DataFrame({ "path" : localization_task } ).reset_index()
-        F = F.join(R.join(R.loc[R["index"].str.contains("^chr.*_bcf$"), "index"].str.extract(r"(?P<chr>chr[^_]+)"), how = "right").set_index("chr").drop(columns = ["index"]).rename(columns = { "path" : "ref_bcf" }), how = "inner")
-        F = F.join(R.join(R.loc[R["index"].str.contains("^chr.*csi$"), "index"].str.extract(r"(?P<chr>chr[^_]+)"), how = "right").set_index("chr").drop(columns = ["index"]).rename(columns = { "path" : "ref_bcf_idx" }), how = "inner")
+            # reference panel BCFs
+            R = pd.DataFrame({ "path" : localization_task } ).reset_index()
+            F = F.join(R.join(R.loc[R["index"].str.contains("^chr.*_bcf$"), "index"].str.extract(r"(?P<chr>chr[^_]+)"), how = "right").set_index("chr").drop(columns = ["index"]).rename(columns = { "path" : "ref_bcf" }), how = "inner")
+            F = F.join(R.join(R.loc[R["index"].str.contains("^chr.*csi$"), "index"].str.extract(r"(?P<chr>chr[^_]+)"), how = "right").set_index("chr").drop(columns = ["index"]).rename(columns = { "path" : "ref_bcf_idx" }), how = "inner")
 
-        return F
+            return F
 
-    F = order_indices(convert_task["bcf"], convert_task["bcf_idx"], localization_task)
+        F = order_indices(convert_task["bcf"], convert_task["bcf_idx"], localization_task)
 
-    #
-    # run Eagle, per chromosome
-    eagle_task = phasing.eagle(
-      inputs = dict(
-        genetic_map_file = localization_task["genetic_map_file"],
-        vcf_in = F["bcf_path"],
-        vcf_idx_in = F["bcf_idx_path"],
-        vcf_ref = F["ref_bcf"],
-        vcf_ref_idx = F["ref_bcf_idx"],
-        output_file_prefix = "foo",
-        num_threads = 1,
-      ),
-      resources = { "cpus-per-task" : 2, "mem":'8G'},
-      outputs = {"phased_vcf" : "foo.vcf"}
-    )
+        #
+        # run Eagle, per chromosome
+        eagle_task = phasing.eagle(
+          inputs = dict(
+            genetic_map_file = localization_task["genetic_map_file"],
+            vcf_in = F["bcf_path"],
+            vcf_idx_in = F["bcf_idx_path"],
+            vcf_ref = F["ref_bcf"],
+            vcf_ref_idx = F["ref_bcf_idx"],
+            output_file_prefix = "foo",
+            num_threads = 1,
+          ),
+          resources = { "cpus-per-task" : 2, "mem":'8G'},
+          outputs = {"phased_vcf" : "foo.vcf"}
+        )
 
-    # combine VCFs
-    combine_vcf_task = wolf.Task(
-      name = "combine_vcfs",
-      inputs = { "vcf_array" : [eagle_task["phased_vcf"]],
-                 "sample_name" : sample_name },
-      script = "bcftools concat -O u $(cat ${vcf_array} | tr '\n' ' ') | bcftools sort -O v -o ${sample_name}_eagle_phased.vcf",
-      outputs = { "combined_vcf" : "*phased.vcf" },
-      docker = "gcr.io/broad-getzlab-workflows/base_image:v0.0.5"
-    )
+        # combine VCFs
+        combine_vcf_task = wolf.Task(
+          name = "combine_vcfs",
+          inputs = { "vcf_array" : [eagle_task["phased_vcf"]],
+                     "sample_name" : sample_name },
+          script = "bcftools concat -O u $(cat ${vcf_array} | tr '\n' ' ') | bcftools sort -O v -o ${sample_name}_eagle_phased.vcf",
+          outputs = { "combined_vcf" : "*phased.vcf" },
+          docker = "gcr.io/broad-getzlab-workflows/base_image:v0.0.5"
+        )
 
-    # preprocess callstats
+        # preprocess callstats
 
-    facets_cs_task = Facets_Preprocess_Callstats(inputs = {
-                                        "callstats": m1_gather["cs_gather"],
-                                        "sample_name": sample_name,
-                                        "db_snp_vcf": localization_task["db_snp_vcf"]
-                                        }
-                                    )
+        facets_cs_task = Facets_Preprocess_Callstats(inputs = {
+                                            "callstats": m1_gather["cs_gather"],
+                                            "sample_name": sample_name,
+                                            "db_snp_vcf": localization_task["db_snp_vcf"]
+                                            }
+                                        )
 
-    ascat_cs_task = ASCAT_Preprocess_Callstats(inputs = {
-                                        "callstats": m1_gather["cs_gather"],
-                                        "sample_name": sample_name,
-                                        "ascat_loci_list": localization_task["ascat_loci_list"]
-                                        }
-                                    )
+        ascat_cs_task = ASCAT_Preprocess_Callstats(inputs = {
+                                            "callstats": m1_gather["cs_gather"],
+                                            "sample_name": sample_name,
+                                            "ascat_loci_list": localization_task["ascat_loci_list"]
+                                            }
+                                        )
     
     if upload_bucket_gs_path is not None:
         # upload files to bucket. upload bucket_path is the ~root of the bucket ~directory
         # of the given sample type. method specific results go in apropriately named subdirs
         upload_root_task = UploadToBucket(files = [m1_gather["cs_gather"],
-                                tumor_cov_gather_task["coverage"],
-                                combine_vcf_task["combined_vcf"]],
+                                tumor_cov_gather_task["coverage"]],
                        bucket = upload_bucket_gs_path
                       )
 
-        upload_hapaseg_task = UploadToBucket(files = [hapaseg_cs_task["hapaseg_hetsite_depths"],
-                                                      hapaseg_cs_task["hapaseg_filtered_cs"],
-                                                      hapaseg_cs_task["hapaseg_genotype"]],
-                       bucket = upload_bucket_gs_path.rstrip('/') + '/hapaseg/'
-                      )
+        if preprocess:
+    
+            upload_phasing_task = UploadToBucket(files = combine_vcf_task["combined_vcf"],
+                           bucket = upload_bucket_gs_path
+                          )
 
-        upload_facets_task = UploadToBucket(files = [facets_cs_task["facets_variant_depths"],
-                                                     facets_cs_task["facets_filtered_variants"]],
-                       bucket = upload_bucket_gs_path.rstrip('/') + '/facets/'
-                      )
-        upload_ascat_task = UploadToBucket(files = [ascat_cs_task["ascat_variant_depths"],
-                                                    ascat_cs_task["ascat_filtered_variants"]],
-                       bucket = upload_bucket_gs_path.rstrip('/') + '/ascat/'
-                      )
+            upload_hapaseg_task = UploadToBucket(files = [hapaseg_cs_task["hapaseg_hetsite_depths"],
+                                                          hapaseg_cs_task["hapaseg_filtered_cs"],
+                                                          hapaseg_cs_task["hapaseg_genotype"]],
+                           bucket = upload_bucket_gs_path.rstrip('/') + '/hapaseg/'
+                          )
 
-    return { "mutect_callstats": m1_gather["cs_gather"],
-             "covcollect": tumor_cov_gather_task["coverage"],
-             "hapaseg_hetsite_depths": hapaseg_cs_task["hapaseg_hetsite_depths"],
-             "hapaseg_filtered_cs": hapaseg_cs_task["hapaseg_filtered_cs"],
-             "facets_variant_depths": facets_cs_task["facets_variant_depths"],
-             "facets_filtered_variants": facets_cs_task["facets_filtered_variants"],
-             "ascat_variant_depths": ascat_cs_task["ascat_variant_depths"],
-             "ascat_filtered_variants": ascat_cs_task["ascat_filtered_variants"],
-             "eagle_phased_vcf": combine_vcf_task["combined_vcf"] 
-            }
+            upload_facets_task = UploadToBucket(files = [facets_cs_task["facets_variant_depths"],
+                                                         facets_cs_task["facets_filtered_variants"]],
+                           bucket = upload_bucket_gs_path.rstrip('/') + '/facets/'
+                          )
+     
+            upload_ascat_task = UploadToBucket(files = [ascat_cs_task["ascat_variant_depths"],
+                                                        ascat_cs_task["ascat_filtered_variants"]],
+                           bucket = upload_bucket_gs_path.rstrip('/') + '/ascat/'
+                          )
 
-
-
-# upload to bucket
-
-# run GATK workflow
 
 # run Hatchet workflow
 
