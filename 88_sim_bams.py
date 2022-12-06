d20d7ae039f6b967b9b6c6f569ec56b297a323a0
Julian Hess <jhess@broadinstitute.org>
2022-12-06 19:18:23 +0000

    Realign RT normals

--- 88_sim_bams.py
+++ 88_sim_bams.py
@@ -44,8 +44,12 @@ WM = dalmatian.WorkspaceManager("broad-firecloud-ibmwatson/Getz_Wu_Richters_WGS_
 S = WM.get_samples()
 WGS_RT = S.loc[S["sample_type"] != "Normal", ["output_bam", "output_bam_index"]]
 
+## RT normals
+S = WM.get_samples()
+WGS_RT_N = S.loc[S["sample_type"] == "Normal", ["output_bam", "output_bam_index"]]
+
 ## now run everything
-with wolf.Workflow(workflow = revert_and_upload, namespace = "hapaseg_sim_realign", max_concurrent_flows = 20, max_concurrent_flow_tasks = 20, scheduler_processes = 3) as w:
+with wolf.Workflow(workflow = revert_and_upload, namespace = "hapaseg_sim_realign", max_concurrent_flows = 20, max_concurrent_flow_tasks = 20, scheduler_processes = 8) as w:
     # TWIST NA12878
     for samp, bam, bai in WES_TWIST.itertuples():
         w.run(
@@ -74,6 +78,18 @@ with wolf.Workflow(workflow = revert_and_upload, namespace = "hapaseg_sim_realig
 
     # FFPE WGS RT/CLL
     for samp, bam, bai in WGS_RT.itertuples():
+        w.run(
+          RUN_NAME = samp, 
+
+          bam = bam,
+          bai = bai,
+          sample_name = samp,
+          bwa_index_dir = "gs://getzlab-workflows-reference_files-oa/hg38/gdc/",
+          n_revert_shards = 30,
+          n_bwa_shards = 50
+        )
+
+    for samp, bam, bai in WGS_RT_N.itertuples():
         w.run(
           RUN_NAME = samp, 
 
