5824812e15973d4e1eba6451af03c6b5ee18f9dd
Oliver Priebe <opriebe@broadinstitute.org>
2023-03-03 22:38:06 +0000

    final plotting functions draft1 complete

--- 90_plot_benchmarking_results.py
+++ 90_plot_benchmarking_results.py
@@ -3,25 +3,119 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import tqdm
+import matplotlib as mpl
 
 def interval_remap(c0, s0, e0, s1, e1):
     return (c0 - s0)/(e0 - s0)*(e1 - s1) + s1
 
-results_files = glob.glob('/mnt/nfs/benchmarking_dir/*_benchmarking_profile*/Downstream*/jobs/0/workspace/*comparison_results.txt')
-results_tups = [(f, *re.search(r".*(FF.*)_benchmarking_profile_(\d+)_\d+_(.*).Downstream_(.*)_Analysis.*", f).groups()) for f in results_files]
-mad_results = [float(open(tups[0], 'r').read().split()[5]) for tups in results_tups]
+def load_mad_results_df(res):
+    mad_results = []
+    for flow in tqdm.tqdm(list(res.keys())):
+        sample_type, complexity, purity = re.search(r"(.*)_benchmarking_profile_(\d+)_\d+_(.*)", flow).groups()
+        downstream_res = [s for s in list(res[flow].keys()) if 'Downstream' in s]
+        for ds in downstream_res:
+            method = ''.join([s for s in re.search(r"Downstream_(.*)_Analysis(_unclustered)?", ds).groups() if s is not None])
+            if res[flow][ds] is not None:
+                mad_path = res[flow][ds].results.outputs.comparison_results[0]
+                # we also want to extract the inferred purity for hapaseg runs
+                if 'HapASeg' in method:
+                    purity_path = res[flow]['Hapaseg_run_acdp'].results.outputs.acdp_optimal_fit_params[0]
+                    inf_purity = float(open(purity_path, 'r').readlines()[1].split('\t')[0])
+                else:
+                    inf_purity = -1
+                mad_results.append((sample_type, complexity, purity, inf_purity, method, float(open(mad_path, 'r').read().split()[5])))
+            else:
+                print("could not load results for ", flow, ds)
+    results_df =  pd.DataFrame(mad_results, columns = ['sample', 'complexity', 'purity', 'inferred_purity', 'method', 'mad_score'])
+    
+    results_df.loc[:, "purity"] = results_df['purity'].astype(float)
+    results_df.loc[:, 'complexity'] = results_df['complexity'].astype(float)
+    results_df.loc[:, 'complexity'] = results_df['complexity'] / 10000
+    results_df['complexity_size'] = interval_remap(results_df['complexity'], results_df['complexity'].min(), results_df['complexity'].max(), 16,128)
+    return results_df
 
-results_df = pd.DataFrame(results_tups, columns = ['path', 'sample', 'complexity', 'purity', 'method']) 
-results_df.purity = results_df.purity.astype(float)
-results_df.loc[:, 'complexity'] = results_df['complexity'].astype(float)
+# old way of gathering results
+#results_files = glob.glob('/mnt/nfs/benchmarking_dir/*_benchmarking_profile*/Downstream*/jobs/0/workspace/*comparison_results.txt')
+#results_tups = [(f, *re.search(r".*(FF.*)_benchmarking_profile_(\d+)_\d+_(.*).Downstream_(.*)_Analysis.*", f).groups()) for f in results_files]
+#mad_results = [float(open(tups[0], 'r').read().split()[5]) for tups in results_tups]
+#results_df = pd.DataFrame(results_tups, columns = ['path', 'sample', 'complexity', 'purity', 'method']) 
 
-results_df.loc[:, 'complexity'] = results_df['complexity'] / 10000
-results_df['mad_score'] = mad_results
-results_df['complexity_size'] = interval_remap(results_df['complexity'], results_df['complexity'].min(), results_df['complexity'].max(), 16,128)
+res = pd.read_pickle('./benchmarking/final_benchmakring_results.pickle')
+results_df = load_mad_results_df(res)
 
-samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032']
-sample_dict = {'FF': 'Fresh Frozen PCR-Free', 'FFPE_CH1032':'FFPE High Quality', 'FFPE_CH1022': 'FFPE Degraded'}
+
+## plot clustered vs unclustered
+samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032', 'exome_TWIST']
+sample_dict = {'FF': 'Fresh Frozen PCR-Free', 'FFPE_CH1032':'FFPE High Quality', 'FFPE_CH1022': 'FFPE Degraded', 'exome_TWIST':'Fresh Frozen WES'}
+methods = ['HapASeg', 'HapASeg_unclustered']
+fig, axs = plt.subplots(4,1, figsize=(6,14), sharex=True)
+ax_lst = axs.flatten()
+colors = plt.get_cmap("tab10")
+color_dict = dict(zip(methods, range(len(methods))))
+marker_dict = dict(zip(methods, ['o', 'x']))
+for i, sample in enumerate(samples):
+    ax = ax_lst[i]
+    ax.set_yscale('log')
+    ax.set_ylabel('MAD')
+    ax.set_xticks([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9], [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
+    for method in methods:
+        res = results_df.loc[(results_df['method'] == method) & (results_df['sample']==sample), ['purity', 'mad_score', 'complexity_size']].sort_values(by='purity').values
+        jit = np.random.rand(len(res[:,0]))/ 20 - 0.025
+        ax.scatter(res[:,0] + jit, res[:,1], color = colors(color_dict[method]), label=method, alpha = 0.3, marker = "o", s = res[:,2])
+
+    for method in methods:
+        means = results_df.loc[(results_df['method'] == method) & (results_df['sample']==sample), ['purity', 'mad_score',]].sort_values(by='purity').groupby('purity').mean()
+        ax.scatter(means.index, means.values, marker=marker_dict[method], color='k')
+    ax.set_title(f'{sample_dict[sample]}')
+ax.set_xlabel('purity')
+plt.legend()
+plt.tight_layout()
+plt.savefig('./final_figures/1_clustered_vs_unclustered.pdf')
+
+## purity vs inferred purity
+fig, axs = plt.subplots(2,2, figsize=(8,8), sharex=True, sharey=True)
+ax_lst = axs.flatten()
+cmap = plt.get_cmap('viridis')
+cnorm = mpl.colors.Normalize(vmin=results_df['complexity'].min(), vmax=results_df['complexity'].max())
+sm = mpl.cm.ScalarMappable(norm=cnorm, cmap=cmap)
+for i, sample in enumerate(samples):
+    ax = ax_lst[i]
+    ax.set_xticks([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9], [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
+    data = results_df.loc[(results_df['sample'] == sample) & (results_df['method'] =='HapASeg'), ['purity', 'inferred_purity', 'complexity']].values
+    jit = np.random.rand(len(data[:,0]))/ 20 - 0.025
+    ax.scatter(data[:,0] + jit, data[:,1], c=data[:,2], cmap=cmap, alpha=0.2)
+    means = results_df.loc[(results_df['sample'] == sample) & (results_df['method'] =='HapASeg'), ['purity', 'inferred_purity']].groupby('purity').mean()
+    ax.scatter(means.index, means.values, marker='x', color='k')
+    ax.plot([0.1,0.9],[0.1,0.9], color='k', alpha=0.3)
+    ax.set_title(f'{sample_dict[sample]}')
+    if i > 1: ax.set_xlabel('purity')
+    if not i % 2 :ax.set_ylabel('inferred purity')
+plt.tight_layout()
+plt.subplots_adjust(bottom=0.1, right=0.85, top=0.95)
+cax = plt.axes([0.9, 0.05, 0.03, 0.9])
+plt.colorbar(sm, cax=cax, label='complexity')
+plt.savefig('./final_figures/2_benchmarking_inferred_purities.pdf')
+
+
+## bubble plots
+
+# set unclustered theshold to 0.3 in wgs and 0.2 in WES
+optimal_df = results_df.copy()
+wgs_samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032']
+optimal_df.loc[(optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity < 0.3) & (optimal_df['method'] == 'HapASeg_unclustered'), 'method'] = 'HapASeg_optimal'
+optimal_df.loc[(optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity >= 0.3) & (optimal_df['method'] == 'HapASeg'), 'method'] = 'HapASeg_optimal'
+optimal_df.loc[(~optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity < 0.2) & (optimal_df['method'] == 'HapASeg_unclustered'), 'method'] = 'HapASeg_optimal'
+optimal_df.loc[(~optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity >= 0.2) & (optimal_df['method'] == 'HapASeg'), 'method'] = 'HapASeg_optimal'
+
+# rename
+opt_methods = ['Facets', 'GATK', 'Hatchet', 'ASCAT', 'HapASeg_optimal']
+optimal_df = optimal_df.loc[optimal_df['method'].isin(opt_methods)]
+optimal_df.loc[optimal_df['method'] == 'HapASeg_optimal', 'method'] = 'HapASeg'
+
 methods = ['HapASeg', 'Facets', 'GATK', 'Hatchet', 'ASCAT']
+colors = plt.get_cmap("tab10")
+color_dict = dict(zip(methods, range(len(methods))))
 
 for sample in samples:
     fig = plt.figure()
@@ -31,12 +125,11 @@ for sample in samples:
     ax.set_xlabel('purity')
     ax.set_xticks([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9], [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
 
-    colors = plt.get_cmap("tab10")
-    color_dict = dict(zip(methods, range(len(methods))))
-    for method in results_df.method.unique():
-        res = results_df.loc[(results_df['method'] == method) & (results_df['sample']==sample), ['purity', 'mad_score', 'complexity_size']].sort_values(by='purity').values
+    for method in methods:
+        res = optimal_df.loc[(optimal_df['method'] == method) & (optimal_df['sample']==sample), ['purity', 'mad_score', 'complexity_size']].sort_values(by='purity').values
         jit = np.random.rand(len(res[:,0]))/ 20 - 0.025 
         ax.scatter(res[:,0] + jit, res[:,1], color = colors(color_dict[method]), label=method, alpha = 0.3, marker = "o", s = res[:,2])
     plt.title(f'{sample_dict[sample]}')
     plt.legend()
-    plt.savefig(f'./{sample}_benchmarking_bubble_plot.pdf')
+    plt.savefig(f'./final_figures/3_{sample}_benchmarking_bubble_plot.pdf')
+
--- 91_benchmarking_heatmap.py
+++ 91_benchmarking_heatmap.py
@@ -1,22 +1,26 @@
 import glob
+import sys
 import re
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import AxesGrid
 import numpy as np
+import tqdm
 
-def extract_segment_results(df_path, sample_name, method, complexity, purity):
+def extract_segment_results(df_path, sample_name, method, complexity, purity, inf_purity):
     df = pd.read_csv(df_path, sep='\t')
     trim_df = df.loc[~df.unique, ['length_overlap', 'major_AD', 'minor_AD', 'major_ccf', 'minor_ccf', 'gt_event_length']]
     trim_df['method'] = method
     trim_df['purity'] = purity
     trim_df['sample'] = sample_name
     trim_df['complexity'] = complexity
+    trim_df['inferred_purity'] = inf_purity
     return trim_df
 
 def create_results_array(seg_results_df, sample_names, methods, purities, length_bins, ccf_bins):
     results_arr = np.zeros(shape=(len(sample_names), len(methods), len(purities), len(ccf_bins) -1, len(length_bins) - 1))
+    counts_arr = np.zeros(shape=(len(sample_names), len(methods), len(purities), len(ccf_bins) -1, len(length_bins) - 1))
     for sample_i, sample in enumerate(sample_names):
         sample_mask = seg_results_df['sample'] == sample
         for method_i, method in enumerate(methods):
@@ -36,58 +40,153 @@ def create_results_array(seg_results_df, sample_names, methods, purities, length
                         else:
                             weighted_avg = np.average(df_res.AD.values, weights=df_res['length_overlap'].values)
                             results_arr[sample_i, method_i, purity_i, ccf_i, len_i] = weighted_avg
-    return results_arr
+                            counts_arr[sample_i, method_i, purity_i, ccf_i, len_i] = len(df_res)
+    return results_arr, counts_arr
 
-segfiles = glob.glob('/mnt/nfs/benchmarking_dir/*_benchmarking_profile_*/Downstream_*/jobs/0/workspace/*_comparison_segfile.tsv')
-segfile_tups = [(f, *re.search(r".*(FF.*)_benchmarking_profile_(\d+)_\d+_(.*).Downstream_(.*)_Analysis.*", f).groups()) for f in segfiles]
-res_dfs = [extract_segment_results(df_path, sample_name, method, int(complexity), float(purity)) for df_path, sample_name, complexity, purity, method in segfile_tups]
-seg_results_df = pd.concat(res_dfs).reset_index(drop=True)
+# old way of gathering results
+#segfiles = glob.glob('/mnt/nfs/benchmarking_dir/*_benchmarking_profile_*/Downstream_*/jobs/0/workspace/*_comparison_segfile.tsv')
+#segfile_tups = [(f, *re.search(r".*(FF.*)_benchmarking_profile_(\d+)_\d+_(.*).Downstream_(.*)_Analysis.*", f).groups()) for f in segfiles]
+#res_dfs = [extract_segment_results(df_path, sample_name, method, int(complexity), float(purity)) for df_path, sample_name, complexity, purity, method in segfile_tups]
+#seg_results_df = pd.concat(res_dfs).reset_index(drop=True)
+#seg_results_df.loc[:, 'complexity'] = seg_results_df['complexity'] / 10000
+#minor_df = seg_results_df.loc[:, ['length_overlap', 'gt_event_length','sample', 'minor_AD', 'minor_ccf', 'method', 'purity', 'complexity']].rename({'minor_AD':'AD', 'minor_ccf':'ccf'}, axis=1)
+#minor_df['allele'] = 'minor'
+#major_df = seg_results_df.loc[:, ['length_overlap','gt_event_length',  'sample', 'major_AD', 'major_ccf', 'method', 'purity', 'complexity']].rename({'major_AD':'AD', 'major_ccf':'ccf'}, axis=1)
+#major_df['allele'] = 'major'
 
-seg_results_df.loc[:, 'complexity'] = seg_results_df['complexity'] / 10000
+def load_seg_results_df(res):
+    res_dfs = []
+    for flow in tqdm.tqdm(list(res.keys())):
+        sample_type, complexity, purity = re.search(r"(.*)_benchmarking_profile_(\d+)_\d+_(.*)", flow).groups()
+        downstream_res = [s for s in list(res[flow].keys()) if 'Downstream' in s]
+        for ds in downstream_res:
+            method = ''.join([s for s in re.search(r"Downstream_(.*)_Analysis(_unclustered)?", ds).groups() if s is not None])
+            if res[flow][ds] is not None:
+                segfile_path = res[flow][ds].results.outputs.comparison_segfile[0]
+                # we also want to extract the inferred purity for hapaseg runs
+                if 'HapASeg' in method:
+                    purity_path = res[flow]['Hapaseg_run_acdp'].results.outputs.acdp_optimal_fit_params[0]
+                    inf_purity = float(open(purity_path, 'r').readlines()[1].split('\t')[0])
+                else:
+                    inf_purity = -1
+                res_dfs.append(extract_segment_results(segfile_path, sample_type, method, int(complexity), float(purity), inf_purity))
+            else:
+                print("could not load results for ", flow, ds)
+    seg_results_df = pd.concat(res_dfs).reset_index(drop=True)
+    seg_results_df.loc[:, 'complexity'] = seg_results_df['complexity'] / 10000
+    minor_df = seg_results_df.loc[:, ['length_overlap', 'gt_event_length','sample', 'minor_AD', 'minor_ccf', 'method', 'purity', 'inferred_purity', 'complexity']].rename({'minor_AD':'AD', 'minor_ccf':'ccf'}, axis=1)
+    minor_df['allele'] = 'minor'
 
-minor_df = seg_results_df.loc[:, ['length_overlap', 'gt_event_length','sample', 'minor_AD', 'minor_ccf', 'method', 'purity', 'complexity']].rename({'minor_AD':'AD', 'minor_ccf':'ccf'}, axis=1)
-minor_df['allele'] = 'minor'
+    major_df = seg_results_df.loc[:, ['length_overlap','gt_event_length',  'sample', 'major_AD', 'major_ccf', 'method', 'purity', 'inferred_purity', 'complexity']].rename({'major_AD':'AD', 'major_ccf':'ccf'}, axis=1)
+    major_df['allele'] = 'major'
+    seg_results_df = pd.concat([minor_df, major_df]).reset_index(drop=True)
+    return seg_results_df
 
-major_df = seg_results_df.loc[:, ['length_overlap','gt_event_length',  'sample', 'major_AD', 'major_ccf', 'method', 'purity', 'complexity']].rename({'major_AD':'AD', 'major_ccf':'ccf'}, axis=1)
-major_df['allele'] = 'major'
+def main():
+    try:
+        flow_results = sys.argv[1]
+        print(f"attempting to load flow results from {flow_results}")
+        res = pd.read_pickle(flow_results)
+    except:
+        print("could not load results pickle")
+        return 
+    
+    seg_results_df = load_seg_results_df(res)
 
-seg_results_df = pd.concat([minor_df, major_df]).reset_index(drop=True)
-length_bins = [1] + [1e5 * 10**i for i in range(0,5)]
-ccf_bins = [0,0.7,.99999,1]
-methods = ['HapASeg', 'ASCAT', 'GATK', 'Facets', 'Hatchet']
-purities = np.r_[0.1:1:0.1]
-samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032']
+    # we set unclustered theshold to 0.3 in wgs and 0.2 in WES
+    optimal_df = seg_results_df.copy()
+    wgs_samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032']
+    optimal_df.loc[(optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity < 0.3) & (optimal_df['method'] == 'HapASeg_unclustered'), 'method'] = 'HapASeg_optimal'
+    optimal_df.loc[(optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity >= 0.3) & (optimal_df['method'] == 'HapASeg'), 'method'] = 'HapASeg_optimal'
+    optimal_df.loc[(~optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity < 0.2) & (optimal_df['method'] == 'HapASeg_unclustered'), 'method'] = 'HapASeg_optimal'
+    optimal_df.loc[(~optimal_df['sample'].isin(wgs_samples)) & (optimal_df.inferred_purity >= 0.2) & (optimal_df['method'] == 'HapASeg'), 'method'] = 'HapASeg_optimal'
 
-results_arr = create_results_array(seg_results_df, samples, methods, purities, length_bins, ccf_bins)
+    # rename
+    opt_methods = ['Facets', 'GATK', 'Hatchet', 'ASCAT', 'HapASeg_optimal']
+    optimal_df = optimal_df.loc[optimal_df['method'].isin(opt_methods)]
+    optimal_df.loc[optimal_df['method'] == 'HapASeg_optimal', 'method'] = 'HapASeg'
 
-plt.set_cmap('turbo')
-fig = plt.figure(figsize=(10,18))
-grid = AxesGrid(fig, (0.05, 0.05, 0.85, 0.9),
-                nrows_ncols=(len(methods),len(samples)),
-                axes_pad=0.01,
-                share_all=False,
-                label_mode="L",
-                cbar_location="right",
-                cbar_mode="single",
+    length_bins = [1] + [5e4 * 10**i for i in range(0,5)]
+    ccf_bins = [0,0.6,.99999,1]
+    methods = ['HapASeg', 'ASCAT', 'GATK', 'Facets', 'Hatchet']
+    purities = np.r_[0.1:1:0.1]
+    samples = ['FF', 'FFPE_CH1022', 'FFPE_CH1032']   
+ 
+    results_arr, counts_arr = create_results_array(optimal_df, samples, methods, purities, length_bins, ccf_bins)
+    #np.savez('heatmap_array_results.npz', results_arr=results_arr, counts_arr=counts_arr)
+
+    plt.set_cmap('turbo')
+    fig = plt.figure(figsize=(10,18))
+    grid = AxesGrid(fig, (0.05, 0.06, 0.85, 0.85),
+                    nrows_ncols=(len(methods),len(samples)),
+                    axes_pad=0.1,
+                    share_all=False,
+                    label_mode="L",
+                    cbar_location="right",
+                    cbar_mode="single",
+                    cbar_pad=0.6    
                 )
-vmin = 0.1
-vmax = 100
-nlb = len(length_bins) - 1
+    vmin = 0.1
+    vmax = 100
+    nlb = len(length_bins) - 1
 
-for row in range(len(methods)):
-    for col in range(len(samples)):
-        ax = grid[row * len(samples) + col]
-        im = ax.imshow(results_arr[col, row].reshape(9,15).T,  norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
-        ax.plot([-0.5,8.5],[nlb - 0.5, nlb - 0.5],linewidth=1.2,color='k')
-        ax.plot([-0.5,8.5],[2*nlb - 0.5, 2 * nlb - 0.5],linewidth=1.2,color='k')
-        if col == 0:
-            ax.set_ylabel(methods[row])
-            ax.set_yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], ['100kb', '1Mb', '10Mb', '100Mb', '1Gb', '100kb', '1Mb', '10Mb', '100Mb', '1Gb', '100kb', '1Mb', '10Mb', '100Mb', '1Gb'], rotation=25, fontsize=4, fontfamily='sans-serif')
+    for row in range(len(methods)):
+        for col in range(len(samples)):
+            ax = grid[row * len(samples) + col]
+            im = ax.imshow(results_arr[col, row].reshape(9,15).T,  norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
+            ax.plot([-0.5,8.5],[nlb - 0.5, nlb - 0.5],linewidth=1.2,color='k')
+            ax.plot([-0.5,8.5],[2*nlb - 0.5, 2 * nlb - 0.5],linewidth=1.2,color='k')
+            if col == 0:
+                ax.set_ylabel(methods[row])
+                ax.set_yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], ['50kb', '500kb', '5Mb', '50Mb', '500Mb', '50kb', '500kb', '5Mb', '50Mb', '500Mb', '50kb', '500kb', '5Mb', '50Mb', '500Mb'], rotation=25, fontsize=4, fontfamily='sans-serif')
+            if col ==2:
+                ax2 = ax.secondary_yaxis('right')
+                ax2.set_yticks([2,7,12], ['low ccf', 'high ccf', 'clonal'], fontsize=7)
+                ax2.yaxis.set_ticks_position('none')
 
-grid[12].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
-grid[13].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
-grid[14].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
-grid.cbar_axes[0].yaxis.set_ticks_position('right')
-grid.cbar_axes[0].colorbar(mpl.cm.ScalarMappable(norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax)))
-plt.show()
-#plt.savefig('/home/opriebe/dev/HapASeg/heatmap_results_plot.pdf')
+    grid[12].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
+    grid[13].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
+    grid[14].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
+    grid.cbar_axes[0].yaxis.set_ticks_position('right')
+    grid.cbar_axes[0].colorbar(mpl.cm.ScalarMappable(norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax)), label='MAD score')
+    grid[0].set_title('Fresh Frozen', rotation=30, fontsize=8, ha='left')
+    grid[1].set_title('FFPE Degraded', rotation=30, fontsize=8, ha='left')
+    grid[2].set_title('Fresh Quality', rotation=30, fontsize=8, ha='left')
+    grid[-2].set_title('purity', y=-0.4, fontsize=8)
+    plt.savefig('./final_figures/4_WGS_heatmap_results_plot.pdf')
+
+    ## for a seperate WES heatmap
+    samples = ['exome_TWIST']
+    exome_results_arr, exome_counts_arr = create_results_array(optimal_df, samples, methods, purities, length_bins, ccf_bins)
+
+    plt.set_cmap('turbo')
+    fig = plt.figure(figsize=(10,18))
+    grid = AxesGrid(fig, (0.05, 0.05, 0.85, 0.9),
+                    nrows_ncols=(len(methods),len(samples)),
+                    axes_pad=0.01,
+                    share_all=False,
+                    label_mode="L",
+                    cbar_location="right",
+                    cbar_mode="single",
+                    )
+    vmin = 0.1
+    vmax = 100
+    nlb = len(length_bins) - 1
+
+    for row in range(len(methods)):
+        for col in range(len(samples)):
+            ax = grid[row * len(samples) + col]
+            im = ax.imshow(results_arr[col, row].reshape(9,15).T,  norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
+            ax.plot([-0.5,8.5],[nlb - 0.5, nlb - 0.5],linewidth=1.2,color='k')
+            ax.plot([-0.5,8.5],[2*nlb - 0.5, 2 * nlb - 0.5],linewidth=1.2,color='k')
+            if col == 0:
+                ax.set_ylabel(methods[row])
+                ax.set_yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], ['50kb', '500kb', '5Mb', '50Mb', '500Mb', '50kb', '500kb', '5Mb', '50Mb', '500Mb', '50kb', '500kb', '5Mb', '50Mb', '500Mb'], rotation=25, fontsize=4, fontfamily='sans-serif')
+
+    grid[4].set_xticks([0,1,2,3,4,5,6,7,8], [0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9], fontsize=4, rotation=90, fontfamily='sans-serif')
+    grid.cbar_axes[0].yaxis.set_ticks_position('right')
+    grid.cbar_axes[0].colorbar(mpl.cm.ScalarMappable(norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax)))
+    plt.savefig('./final_figures/4_WES_heatmap_results_plot.pdf')
+
+if __name__ == "__main__":
+    main()
