fc0707c572268388b21acbd24fac1479975d1a4e
Ron Solan <rsolan@broadinstitute.org>
2025-10-02 19:26:27 -0400

    ...

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -401,7 +401,7 @@ class Hapaseg_run_acdp(wolf.Task):
         "acdp_optimal_fit_params": "acdp_optimal_fit_params.txt",
     }
 
-    docker = "gcr.io/broad-getzlab-workflows/hapaseg:exclusion_bed_2_v1312"
+    docker = "gcr.io/broad-getzlab-workflows/hapaseg:exclusion_bed_2_v1313"
     resources = {"cpus-per-task": 4, "mem": "6G"}
 
 
